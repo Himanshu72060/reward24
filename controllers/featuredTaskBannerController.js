2eb0aba609795a1c9aa100a4b2e5a0841cd80348
Himanshu72060 <himanshusharma00898@gmail.com>
2026-05-30 17:23:04 +0530

    fix admin route 404

--- controllers/featuredTaskBannerController.js
+++ controllers/featuredTaskBannerController.js
@@ -1,168 +1,125 @@
+const fs =
+    require("fs");
+
 const FeaturedTaskBanner =
     require(
         "../models/FeaturedTaskBanner"
     );
 
-// CREATE
+const uploadToBunny =
+    require(
+        "../utils/bunnyUpload"
+    );
 
 exports.createBanner =
-    async (req, res) => {
+    async (
+        req,
+        res
+    ) => {
 
         try {
+
+            let backgroundImageUrl =
+                "";
+
+            let taskImageUrl =
+                "";
+
+            if (
+                req.files
+                    ?.backgroundImage
+            ) {
+
+                const file =
+                    req.files
+                        .backgroundImage[0];
+
+                backgroundImageUrl =
+                    await uploadToBunny(
+                        file.path,
+                        Date.now() +
+                        "-" +
+                        file.originalname
+                    );
+
+                fs.unlinkSync(
+                    file.path
+                );
+            }
+
+            if (
+                req.files
+                    ?.taskImage
+            ) {
+
+                const file =
+                    req.files
+                        .taskImage[0];
+
+                taskImageUrl =
+                    await uploadToBunny(
+                        file.path,
+                        Date.now() +
+                        "-" +
+                        file.originalname
+                    );
+
+                fs.unlinkSync(
+                    file.path
+                );
+            }
 
             const banner =
                 await FeaturedTaskBanner.create(
-                    req.body
+                    {
+                        tagText:
+                            req.body
+                                .tagText,
+
+                        title:
+                            req.body
+                                .title,
+
+                        subtitle:
+                            req.body
+                                .subtitle,
+
+                        backgroundImageUrl,
+
+                        taskImageUrl,
+
+                        startColorHex:
+                            req.body
+                                .startColorHex,
+
+                        endColorHex:
+                            req.body
+                                .endColorHex,
+
+                        tagColorHex:
+                            req.body
+                                .tagColorHex,
+
+                        buttonText:
+                            req.body
+                                .buttonText
+                    }
                 );
 
-            res.status(201).json({
-                success: true,
-                data: banner
-            });
+            res.status(201)
+                .json({
+                    success: true,
+                    data: banner
+                });
 
         } catch (error) {
 
-            res.status(500).json({
-                success: false,
-                message:
-                    error.message
-            });
-
-        }
-
-    };
-
-// GET ALL
-
-exports.getBanners =
-    async (req, res) => {
-
-        try {
-
-            const banners =
-                await FeaturedTaskBanner
-                    .find()
-                    .sort({
-                        createdAt: -1
-                    });
-
-            res.status(200).json({
-                success: true,
-                data: banners
-            });
-
-        } catch (error) {
-
-            res.status(500).json({
-                success: false,
-                message:
-                    error.message
-            });
-
-        }
-
-    };
-
-// GET SINGLE
-
-exports.getBanner =
-    async (req, res) => {
-
-        try {
-
-            const banner =
-                await FeaturedTaskBanner
-                    .findById(
-                        req.params.id
-                    );
-
-            if (!banner) {
-
-                return res.status(404)
-                    .json({
-                        success: false,
-                        message:
-                            "Banner not found"
-                    });
-
-            }
-
-            res.status(200).json({
-                success: true,
-                data: banner
-            });
-
-        } catch (error) {
-
-            res.status(500).json({
-                success: false,
-                message:
-                    error.message
-            });
-
-        }
-
-    };
-
-// UPDATE
-
-exports.updateBanner =
-    async (req, res) => {
-
-        try {
-
-            const banner =
-                await FeaturedTaskBanner
-                    .findByIdAndUpdate(
-                        req.params.id,
-                        req.body,
-                        {
-                            new: true
-                        }
-                    );
-
-            res.status(200).json({
-                success: true,
-                data: banner
-            });
-
-        } catch (error) {
-
-            res.status(500).json({
-                success: false,
-                message:
-                    error.message
-            });
-
-        }
-
-    };
-
-// DELETE
-
-exports.deleteBanner =
-    async (req, res) => {
-
-        try {
-
-            await FeaturedTaskBanner
-                .findByIdAndDelete(
-                    req.params.id
-                );
-
-            res.status(200).json({
-                success: true,
-                message:
-                    "Banner Deleted"
-            });
-
-        } catch (error) {
-
-            res.status(500).json({
-                success: false,
-                message:
-                    error.message
-            });
+            res.status(500)
+                .json({
+                    success: false,
+                    message:
+                        error.message
+                });
 
         }
 
